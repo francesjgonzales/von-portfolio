62e2d9d77598dcee961d393b738941818523ea1b
francesjgonzales <francesj7837@gmail.com>
2022-12-30 23:25:28 +0800

    added content in works page

--- src/components/work/workData.js
+++ src/components/work/workData.js
@@ -1,51 +1,46 @@
 const workData = [
     {
       id: 1,
-      cardTitle: 'Large-scale banners',
-      cardImage: 'assets/Von-banners.png',
-      cardText: 'Work Description 1test',
+      cardTitle: 'Commercial Print',
+      cardImage: 'assets/Von-banners.jpg',
+      cardText: 'Works from marketing collaterals to large-scale print',
       cardLink: '/worksDesc/1',
     },
     {
       id: 2,
       cardTitle: 'Publishing',
-      cardImage: 'assets/Von-books.png',
-      cardText: 'Work Description 2',
+      cardImage: 'assets/Von-books.jpg',
+      cardText: 'Projects include coffee-table books, magazines & newsletters',
       cardLink: '/worksDesc/2',
     },
     {
+      id: 3,
       cardTitle: 'Branding',
-      cardImage: 'assets/Von-branding.png',
-      cardText: 'Work Description 3',
+      cardImage: 'assets/Von-branding.jpg',
+      cardText: 'Key visual adaptation, Conceptual Design',
       cardLink: 'Link 2'
     },
     {
-      cardTitle: 'Work 3',
-      cardText: 'Work Description 3',
+      id: 4,
+      cardTitle: 'Digital',
+      cardImage: 'assets/Von-digital3.jpg',
+      cardText: 'Design & adaptation of web banners & social media posts',
       cardLink: 'Link 2'
     },
     {
-      cardTitle: 'Work 3',
-      cardText: 'Work Description 3',
+      id: 5,
+      cardTitle: 'Motion Graphics',
+      cardImage: 'assets/Von-motion.jpg',
+      cardText: 'Animation for web banners and social media posts',
       cardLink: 'Link 2'
     },
     {
-      cardTitle: 'Work 3',
-      cardText: 'Work Description 3',
+      id: 6,
+      cardTitle: 'Web Design',
+      cardImage: 'assets/Von-web.jpg',
+      cardText: 'Wireframe and UI component design',
       cardLink: 'Link 2'
-    },
-    {
-      cardTitle: 'Work 3',
-      cardText: 'Work Description 3',
-      cardLink: 'Link 2'
-    },
-    {
-      cardTitle: 'Work 3',
-      cardText: 'Work Description 3',
-      cardLink: 'Link 2'
-    },
-    
-
+    }
   ]
 
   export default workData;
