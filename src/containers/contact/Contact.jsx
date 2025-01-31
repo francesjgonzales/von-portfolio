f47e13e12a7ccd53785b718ff71872da31a3c2c5
francesjgonzales <francesj7837@gmail.com>
2025-01-30 18:43:42 -0700

    Fixed broken links

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import './contact.css';
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import "./contact.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Contact = () => {
   return (
-    <Container className='pt-4 d-flex justify-content-center'>
-      <Card style={{ width: '25rem' }}>
+    <Container className="pt-4 d-flex justify-content-center">
+      <Card style={{ width: "50rem" }}>
         <Card.Header>Contact me!</Card.Header>
-        <Card.Body>
+
+        <Card.Body className="m-5">
           <Card.Title>Von Gonzales</Card.Title>
-          <Card.Text> Senior Designer</Card.Text>
+          <Card.Text> Web Designer</Card.Text>
+
           <Card.Text>
-            Drop me an email at dunhillvongonzales@gmail.com 
+            Drop me an email at dunhillvongonzales@gmail.com
           </Card.Text>
-          <a href="mailto:dunhillvongonzales@gmail.com"> <Button variant="primary">Send email</Button></a>
+          <a href="mailto:dunhillvongonzales@gmail.com">
+            {" "}
+            <Button variant="primary">Send email</Button>
+          </a>
         </Card.Body>
       </Card>
     </Container>
+  );
+};
 
-  )
-}
-
-export default Contact
+export default Contact;
